ff69890d4bcee100e99ba7b0082ae1d575bd248f
29shrey <114428925+29shrey@users.noreply.github.com>
2023-12-17 17:43:58 +0530

    Update hierarchical_clustering.R

--- hierarchical_clustering.R
+++ hierarchical_clustering.R
@@ -4,12 +4,12 @@ data <- data.frame(
   x=c(1,1,7,4,6,5,1,4,3),
   y=c(9,6,3,8,6,5,2,9,9)
 )
-plot(x, y, col = "red", pch = 19,cex=2)
+plot(x, y, col = "red", pch = 19)
 d_mat <- dist(data,method = "euclidean")
 d_mat
 model <- hclust (d_mat,method = "average")
 summary(model)
 plot(model)
-rect.hclust(model,k=3)
+#rect.hclust(model,k=3)
 fit <- cutree(model,k=3)
 print(fit)

e952557c68c2fbe2b6c64eefee648d1caf85d01a
29shrey <114428925+29shrey@users.noreply.github.com>
2023-12-17 15:45:22 +0530

    Update kmeans.R

--- kmeans.R
+++ kmeans.R
@@ -1,49 +1,30 @@
-# Installing Packages 
 install.packages("ClusterR") 
 install.packages("cluster")
 
-# Loading package 
-library(ClusterR) 
-library(cluster) 
+#loading
+library(ClusterR)
+library(cluster)
 
-# Loading data 
-data(iris) 
+#dataframe
+df <- data.frame(x = c(2,2,8,5,7,6,1,4,3),
+                 y = c(10,5,4,8,5,4,2,9,9))
 
-# Structure 
-str(iris)
+#structure
+str(df)
 
-
-# Removing initial label of  
-# Species from original dataset
-iris_1 <- iris[, -5] 
-
-# Fitting K-Means clustering Model  
-# to training dataset 
-set.seed(240) # Setting seed 
-kmeans.re <- kmeans(iris_1, centers = 3, nstart = 20) 
-kmeans.re 
-
-# Cluster identification for  
-# each observation 
-kmeans.re$cluster 
-
-# Confusion Matrix 
-cm <- table(iris$Species, kmeans.re$cluster) 
-cm 
+#fitting
+kmeans_r <- kmeans(df, centers = 2) 
+kmeans_r
 
 # Model Evaluation and visualization 
-plot(iris_1[c("Sepal.Length", "Sepal.Width")],  
-     col = kmeans.re$cluster,
+plot(df[c("x", "y")],  
+     col = kmeans_r$cluster,
      pch=19,
-     main = "K-means with 3 clusters") 
-
-## Plotiing cluster centers 
-kmeans.re$centers 
-kmeans.re$centers[, c("Sepal.Length", "Sepal.Width")] 
+     main = "K-means with 2 clusters") 
 
 ## Visualizing clusters 
-y_kmeans <- kmeans.re$cluster 
-clusplot(iris_1[, c("Sepal.Length", "Sepal.Width")], 
+y_kmeans <- kmeans_r$cluster 
+clusplot(df[c("x","y")], 
          y_kmeans, 
          lines = 0, 
          shade = TRUE, 
@@ -51,6 +32,6 @@ clusplot(iris_1[, c("Sepal.Length", "Sepal.Width")],
          labels = 2, 
          plotchar = FALSE, 
          span = TRUE, 
-         main = paste("Cluster iris"), 
-         xlab = 'Sepal.Length', 
-         ylab = 'Sepal.Width') 
+         main = paste("Cluster X Y"), 
+         xlab = 'X', 
+         ylab = 'Y')
